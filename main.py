eb4ccd7855ed2bf70445fc8507042f4b6403c8bb
orangejuicesucker <29vlahakisa@wiltonps.org>
2024-09-24 14:05:17 +0000

    error with comabt btw

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import time
+import random
+for i in range(100):
+        print(" ")
+
 print("insert name")
 playername = input()
 print("loading")
 time.sleep(5)
 print("type anything to begin")
-cheatenable = input()
-if cheatenable == "enablecheats":
+if input() == "enablecheats":
     print("cheats enabled")
     for i in range(100):
         print(" ")
@@ -23,12 +26,91 @@ if cheatenable == "enablecheats":
     charisma = input()
     for i in range(100):
         print(" ")
-    print("stats:")
-    print(strength)
-    print(dexterity)
-    print(constitution)
-    print(intelligence)
-    print(wisdom)
-    print(charisma)
+    print("--stats--")
+    print("strength =", strength)
+    print("dexterity =", dexterity)
+    print("constitution =", constitution)
+    print("intelligence =", intelligence)
+    print("charisma =", charisma)
 else:
-    print("skibidi")
+    for i in range(100):
+        print(" ")
+    print("--stats--")
+    strength = (random.randrange(1, 20, 3))
+    print("strength =", strength)
+    dexterity = (random.randrange(1, 20, 3))
+    print("dexterity =", dexterity)
+    constitution = (random.randrange(1, 20, 3))
+    print("constitution =", constitution)
+    intelligence = (random.randrange(1, 20, 3))
+    print("intelligence =", intelligence)
+    charisma = (random.randrange(1, 20, 3))
+    print("charisma =", charisma)
+
+time.sleep(5)
+for i in range(100):
+        print(" ")
+print("loading...")
+def Battle(enemyname: int, ac: int, df: int, at: int):
+     for i in range(100):
+        print(" ")
+     print("--",enemyname,"--")
+     print("-",ac,"armor class-", "-",df,"hp-", "-",at,"attack-")
+     print("")
+     print("-fight- -equiment-")
+     if input == "fight":
+          print("--",weapon,"--")
+          if input == weapon:
+               damage = strength + weaponbonusattack
+               df = df-damage
+               print("")
+               print("you attack the enemy dealing",damage," damage!")
+
+time.sleep(5)
+for i in range(100):
+        print(" ")
+print("welcome", playername, "to (UNTITLED RPG 1#)")
+print("")
+print("--please select a weapon from the list below--")
+print("")
+print("sword -- +1 damage")
+print("sheild -- +1 defense")
+weapon = input()
+if weapon == "sword":
+    weaponbonusattack = 1
+    weaponbonusdefense = 0
+    print("")
+    print("1+ damage!")
+elif weapon == "sheild":
+     weaponbonusattack = 0
+     weaponbonusdefense = 1
+     weapon = "fists"
+     print("")
+     print("+1 defense!")
+else:
+     print("no weapon selected")
+     weaponbonusattack = 0
+     weaponbonusdefense = 0
+     weapon = "fists"
+print("")
+print("setup complete! now loading...")
+time.sleep(5)
+for i in range(100):
+        print(" ")
+print("--Welcome to (debugworld)--")
+print("")
+print("you see a path ahead of you, a (test dummy) to your left, and a chest to your right")
+print("")
+print("--go ahead, go left, go right--")
+if input() == "go left":
+    Battle("dummy")
+    for i in range(100):
+        print(" ")
+    print("DUMMY APROACHES!")
+    time.sleep(2)
+    for i in range(100):
+        print(" ")
+    print("loading")
+    time.sleep(2)
+    Battle("dummy", 5, 10, 1)
+    print("g")
